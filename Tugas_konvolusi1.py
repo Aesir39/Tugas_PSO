96e1c0c93c606bcd56468a29f841d6a981133dd3
Aesir39 <galeganteng@gmail.com>
2023-09-15 19:46:40 +0700

    Adding project to GitHub repo

--- Tugas_konvolusi1.py
+++ Tugas_konvolusi1.py
@@ -10,21 +10,16 @@ def conv2d(image, kernel):
     image_height, image_width = image.shape
     kernel_height, kernel_width = kernel.shape
     
-    # Calculate the output size
     output_height = image_height - kernel_height + 1
     output_width = image_width - kernel_width + 1
-    
-    # Initialize the output matrix
     output = np.zeros((output_height, output_width))
     
-    # Perform 2D convolution
     for i in range(output_height):
         for j in range(output_width):
             output[i, j] = np.sum(image[i:i+kernel_height, j:j+kernel_width] * kernel)
     
     return output
 
-# Define the input matrix x (user input)
 x_rows = int(input("Enter the number of rows for the input matrix x: "))
 x_cols = int(input("Enter the number of columns for the input matrix x: "))
 
@@ -33,7 +28,6 @@ x = np.zeros((x_rows, x_cols))
 for i in range(x_rows):
     x[i] = list(map(float, input().split()))
 
-# Define the input kernel h (user input)
 h_rows = int(input("Enter the number of rows for the kernel h: "))
 h_cols = int(input("Enter the number of columns for the kernel h: "))
 
@@ -42,10 +36,8 @@ h = np.zeros((h_rows, h_cols))
 for i in range(h_rows):
     h[i] = list(map(float, input().split()))
 
-# Perform 2D convolution
 result = conv2d(x, h)
 
-# Print the result
 print("Input Image (x):")
 print(x)
 print("\nKernel (h):")
